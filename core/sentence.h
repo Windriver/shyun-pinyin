218d06014066eba22708709f5cbf62fc9ca2e00a
windriver <windriver@home-pc.vm>
2013-09-21 19:55:31 +0800

    sentence

---
+++ core/sentence.h
@@ -0,0 +1,9 @@
+// Copyright 2013, ZhongYi Inc.
+// Author: Windriver < http://windriver.github.io >
+
+#ifndef SHYUN-PINYIN_CORE_SENTENCE_H_
+#define SHYUN-PINYIN_CORE_SENTENCE_H_
+#pragma once
+
+#endif  // SHYUN-PINYIN_CORE_SENTENCE_H_
+
